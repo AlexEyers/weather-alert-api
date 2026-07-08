208e6f1f6bb50e330aa854ab934e7ba28befed89
Alex Eyers <alexander.eyers@gmail.com>
2026-07-08 18:57:32 +0100

    Completed WeatherService to call WeatherApiClient and then get the response and return it as a CurrentWeatherResponse. Next step is Controller

---
+++ src/main/java/com/alex/weather_alert_api/service/WeatherService.java
@@ -0,0 +1,29 @@
+package com.alex.weather_alert_api.service;
+
+import com.alex.weather_alert_api.client.WeatherApiClient;
+import com.alex.weather_alert_api.dto.response.CurrentWeatherResponse;
+import com.alex.weather_alert_api.dto.weatherapi.WeatherApiCurrentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service // Same as @Component but with clearer intent for the dev
+@RequiredArgsConstructor //
+public class WeatherService {
+
+    private final WeatherApiClient weatherApiClient;
+
+    public CurrentWeatherResponse getCurrentWeather(String location) {
+        WeatherApiCurrentResponse weatherApiCurrentResponse =
+                weatherApiClient.getCurrentWeatherByLocation(location);
+
+        return new CurrentWeatherResponse(
+                weatherApiCurrentResponse.getLocation().getName(),
+                weatherApiCurrentResponse.getLocation().getCountry(),
+                weatherApiCurrentResponse.getLocation().getLocalTime(),
+                weatherApiCurrentResponse.getCurrent().getTempC(),
+                weatherApiCurrentResponse.getCurrent().getFeelsLikeC(),
+                weatherApiCurrentResponse.getCurrent().getHumidity(),
+                weatherApiCurrentResponse.getCurrent().getCondition().getText()
+        );
+    }
+}
